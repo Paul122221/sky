25ffe2a89e84b4b6cf6ae3ecc001d3670cbbec2c
Pavel <exesellentmen@yandex.ru>
2024-03-26 12:46:01 +0300

    🚀 Creating the core of the project

---
+++ src/main/java/com/paul/demo/ChatApplication.java
@@ -0,0 +1,11 @@
+package com.paul.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+}
